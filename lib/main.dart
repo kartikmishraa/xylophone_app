af5da80b563bfe176bdaf7485670ab320f96e962
Kartik Mishra <kartikm2219@gmail.com>
2022-02-17 22:52:03 +0530

    modularized the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,52 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: title_,
-      home: XylophoneApp(),
+      home: SplashPage(), //XylophoneApp(),
     );
-  }
-}
-
-class XylophoneApp extends StatefulWidget {
-  const XylophoneApp({ Key? key }) : super(key: key);
-
-  @override
-  State<XylophoneApp> createState() => _XylophoneAppState();
-}
-
-class _XylophoneAppState extends State<XylophoneApp> {
-
-  void playMedia(int audioNum) {
-    final audioPlayer = AudioCache();
-    audioPlayer.play('note$audioNum.wav');
-  }
-
-  Expanded xyloKey(int audioNum, Color _color) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () {playMedia(audioNum);},
-        child: Container(
-          color: _color,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            xyloKey(1, Colors.red),
-            xyloKey(2, Colors.orange),
-            xyloKey(3, Colors.blue),
-            xyloKey(4, Colors.pink),
-            xyloKey(5, Colors.greenAccent),
-            xyloKey(6, Colors.limeAccent),
-            xyloKey(7, Colors.deepPurpleAccent),
-          ],
-        ),
-      ),
-      );
   }
 }
